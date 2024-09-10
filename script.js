b6ca8446a0335913354fe558f9294eec11e960d7
DegreeControl <info@degreecontrol.ca>
2024-09-10 07:16:04 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-const btn = document.getElementById('contact_form_button');
-const send_message = document.getElementById('send_message');
-
-document.getElementById('contact_form')
-    .addEventListener('submit', function (event) {
-        event.preventDefault();
-
-        btn.value = 'SENDING...';
-
-        const serviceID = 'service_us8ae4i';
-        const templateID = 'template_i4qo34c';
-        console.log(this);
-
-        emailjs.sendForm(serviceID, templateID, this)
-            .then(() => {
-                send_message.value = 'Thank you for your message! Our team members will contact you soon.';
-            }, (err) => {
-                send_message.value = 'Please provide all information so that we can better help you!';
-            });
-    });
-
 var scrollto = function(id){
     var reqId = "#" + id;
     window.scrollTo(0, $(reqId).offset().top-85);
